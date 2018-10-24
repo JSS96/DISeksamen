8b224c33e1b6fc46165ba33a038c57dbd4d28d47
Jonas Steen <Jonas1@Jonass-MacBook-Pro.local>
2018-10-24 14:48:44 +0200

    Added a different salt to hashing

--- src/main/java/utils/Hashing.java
+++ src/main/java/utils/Hashing.java
@@ -7,68 +7,65 @@ import org.bouncycastle.util.encoders.Hex;
 
 public final class Hashing {
 
-  // TODO: You should add a salt and make this secure Fixed
-  private String salt;
+    // TODO: You should add a salt and make this secure Fixed
 
-  public void setSalt(String salt){
-    this.salt="jbashf";
-  }
+    public String md5WithSalt(String string) {
 
-  public String md5WithSalt(String string){
+//     Nedenfor sætter jeg mit salt og password sammen. Salten består at et unix timestamp og en streng jeg selv har lavet
+        string = System.currentTimeMillis() + string + "HdasdBDioahsdbDoaw747428";
 
-    string = this.salt+ string;
-    
-    return md5(string);
-  }
-
-  public static String md5(String rawString) {
-    try {
-
-      // We load the hashing algoritm we wish to use.
-      MessageDigest md = MessageDigest.getInstance("MD5");
-
-      // We convert to byte array
-      byte[] byteArray = md.digest(rawString.getBytes());
-
-      // Initialize a string buffer
-      StringBuffer sb = new StringBuffer();
-
-      // Run through byteArray one element at a time and append the value to our stringBuffer
-      for (int i = 0; i < byteArray.length; ++i) {
-        sb.append(Integer.toHexString((byteArray[i] & 0xFF) | 0x100).substring(1, 3));
-      }
-
-      //Convert back to a single string and return
-      return sb.toString();
-
-    } catch (java.security.NoSuchAlgorithmException e) {
-
-      //If somethings breaks
-      System.out.println("Could not hash string");
+//      Nedenfor hashes password samt mit salt
+        return md5(string);
     }
 
-    return null;
-  }
+    public static String md5(String rawString) {
+        try {
 
-  // TODO: You should add a salt and make this secure
-  public static String sha(String rawString) {
-    try {
-      // We load the hashing algoritm we wish to use.
-      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            // We load the hashing algoritm we wish to use.
+            MessageDigest md = MessageDigest.getInstance("MD5");
 
-      // We convert to byte array
-      byte[] hash = digest.digest(rawString.getBytes(StandardCharsets.UTF_8));
+            // We convert to byte array
+            byte[] byteArray = md.digest(rawString.getBytes());
 
-      // We create the hashed string
-      String sha256hex = new String(Hex.encode(hash));
+            // Initialize a string buffer
+            StringBuffer sb = new StringBuffer();
 
-      // And return the string
-      return sha256hex;
+            // Run through byteArray one element at a time and append the value to our stringBuffer
+            for (int i = 0; i < byteArray.length; ++i) {
+                sb.append(Integer.toHexString((byteArray[i] & 0xFF) | 0x100).substring(1, 3));
+            }
 
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
+            //Convert back to a single string and return
+            return sb.toString();
+
+        } catch (java.security.NoSuchAlgorithmException e) {
+
+            //If somethings breaks
+            System.out.println("Could not hash string");
+        }
+
+        return null;
     }
 
-    return rawString;
-  }
+    // TODO: You should add a salt and make this secure (Uses MD5)
+    public static String sha(String rawString) {
+        try {
+            // We load the hashing algoritm we wish to use.
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            // We convert to byte array
+            byte[] hash = digest.digest(rawString.getBytes(StandardCharsets.UTF_8));
+
+            // We create the hashed string
+            String sha256hex = new String(Hex.encode(hash));
+
+            // And return the string
+            return sha256hex;
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return rawString;
+    }
 }
